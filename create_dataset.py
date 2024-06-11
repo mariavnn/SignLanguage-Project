361384cf3931a3c8095d040d43ab381ccebff7d1
Gabriela Espinosa <espinosaalonsomariagabriela8@gmail.com>
2024-06-11 13:35:25 -0500

    se agrego mas letras a la data

--- create_dataset.py
+++ create_dataset.py
@@ -1,8 +1,8 @@
 import os
+import pickle
 import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
-import pickle
 
 
 mp_hands = mp.solutions.hands
@@ -11,30 +11,29 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-DATA_DIR = './data'
+DATA_DIR = "./data"
 
 data = []
 labels = []
 
 for dir_ in os.listdir(DATA_DIR):
     for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
-        data_aux  = []
+        data_aux = []
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        
+
         result = hands.process(img_rgb)
         if result.multi_hand_landmarks:
             for hand_landmarks in result.multi_hand_landmarks:
-               for i in range(len(hand_landmarks.landmark)):
-                   x = hand_landmarks.landmark[i].x
-                   y = hand_landmarks.landmark[i].y
-                   data_aux.append(x)
-                   data_aux.append(y)
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
 
             data.append(data_aux)
             labels.append(dir_)
-  
-f = open('data.pickle', 'wb')
-pickle.dump({'data': data, 'labels': labels}, f)
-f.close()
 
+f = open("data.pickle", "wb")
+pickle.dump({"data": data, "labels": labels}, f)
+f.close()
--- inference_classifier.py
+++ inference_classifier.py
@@ -16,7 +16,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
-labels_dict = {0: 'A', 1:'B', 2: 'L' }
+labels_dict = {0: 'A', 1:'B', 2: 'C', 3: 'D', 4: 'E'}
 
 while True:
     data_aux = []
