0acb206eeb63c1048b331db6b2590e28ea5e4cfd
Gabriela Espinosa <espinosaalonsomariagabriela8@gmail.com>
2024-06-17 21:10:26 -0500

    ft:collect data

--- collectData.py
+++ collectData.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 
-DATA_DIR = './data'
+DATA_DIR = "./data"
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
@@ -19,22 +19,31 @@ for j in range(number_of_classes):
     os.makedirs(class_dir)
     next_index += 1
 
-    print(f'Collecting data for class {j} in {class_dir}')
+    print(f"Collecting data for class {j} in {class_dir}")
 
     done = False
     while True:
         ret, frame = cap.read()
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(25) == ord('q'):
+        cv2.putText(
+            frame,
+            'Ready? Press "Q" ! :)',
+            (100, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1.3,
+            (0, 255, 0),
+            3,
+            cv2.LINE_AA,
+        )
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(25) == ord("q"):
             break
 
     counter = 0
     while counter < dataset_size:
         ret, frame = cap.read()
-        cv2.imshow('frame', frame)
+        cv2.imshow("frame", frame)
         cv2.waitKey(25)
-        cv2.imwrite(os.path.join(class_dir, f'{counter}.jpg'), frame)
+        cv2.imwrite(os.path.join(class_dir, f"{counter}.jpg"), frame)
         counter += 1
 
 cap.release()
